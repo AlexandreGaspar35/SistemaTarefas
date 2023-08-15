d38a55f91e40937ee0c20e16a37d89974a2d0c16
Alexandre <alexandre.d.s.gaspar@gmail.com>
2023-08-15 19:29:59 -0300

    Criando Controller de Usuario

--- SistemaTarefas/Models/TarefaModel.cs
+++ SistemaTarefas/Models/TarefaModel.cs
@@ -2,5 +2,12 @@
 {
     public class TarefaModel
     {
+        public int Id { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public int Status { get; set; }
     }
 }
