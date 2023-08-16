5cded49b86e7a9d20c1e13f7a464075fcacac67a
Alexandre <alexandre.d.s.gaspar@gmail.com>
2023-08-16 19:51:33 -0300

    Criando Construtor e Injetando as Dependencias (Get; Post; GetId)

--- SistemaTarefas/Controllers/UsuarioController.cs
+++ SistemaTarefas/Controllers/UsuarioController.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SistemaTarefas.Models;
+using SistemaTarefas.Repositorios.Interfaces;
 
 namespace SistemaTarefas.Controllers
 {
@@ -8,11 +8,33 @@ namespace SistemaTarefas.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<UsuarioModel>> BuscarTodosUsuario()
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
         {
-            return Ok();
+            _usuarioRepositorio = usuarioRepositorio;
+
         }
-            
+        [HttpGet]
+        public async Task<ActionResult<List<UsuarioModel>>> BuscarTodosUsuario()
+        {
+            List<UsuarioModel> usuarios = await _usuarioRepositorio.BuscarTodosUsuarios();
+            return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UsuarioModel>> BuscarPorId(int id)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.BuscarPorId(id);
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuarioModel)
+        {
+           UsuarioModel usuario =  await _usuarioRepositorio.Adicionar(usuarioModel);
+            return Ok(usuario);
+        }
+
+
     }
 }
